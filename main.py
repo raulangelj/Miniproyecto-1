c83b9396c6707971950badb94355f2fc595f83b8
Raúl Angel Jiménez <raulangelj@gmail.com>
2022-08-21 15:30:11 -0600

    chore: : exercise 1.3 done

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from scipy.stats import poisson
 
 
 # %%
-def poisson_distribution(k, mu, decimals=5):
+def poisson_pmf(k, mu, decimals=5):
 	k = np.arange(0, k)
 	pmf = poisson.pmf(k, mu=mu)
 	pmf = np.round(pmf, decimals)
@@ -24,8 +24,25 @@ def poisson_distribution(k, mu, decimals=5):
 	plt.show()
 
 # %%
+def poisson_cdf(k, mu, decimals=5):
+	k = np.arange(0, k)
+	cdf = poisson.cdf(k, mu=mu)
+	cdf = np.round(cdf, decimals)
+	for val, prob in zip(k,cdf):
+		print(f"k-value {val} (P<={val}) has probability = {prob}")
+	plt.plot(k, cdf, marker='o')
+	plt.xlabel('i')
+	plt.ylabel('Cumulative Probability')
+	plt.show()
+
+# %%
 # ejercicio 1.2
 # 2. Considere que usted analizará hasta un máximo de 16 huracanes este año. Grafique PMF (probability mass function) de estos eventos
 
-poisson_distribution(16, mu=7)
+poisson_pmf(16, mu=7)
+# %%
+# Ejercicio 1.3
+# 3. Considere que usted analizará hasta un máximo de 16 huracanes este año. Grafique CDF (cumulative distribution function) de estos eventos
+
+poisson_cdf(16, mu=7)
 # %%
